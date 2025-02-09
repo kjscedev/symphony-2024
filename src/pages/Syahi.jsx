cad3644d29671fb460d0e03f47c5866e589c9c1a
kjscedev <kjscelive@somaiya.edu>
2025-02-09 14:25:52 +0530

    syahi mobile fix

--- src/pages/Syahi.jsx
+++ src/pages/Syahi.jsx
@@ -3,12 +3,12 @@ function syahi() {
     <div className="h-[100vh] w-[100vw] flex justify-center pt-[12vh] font-Symphony"
     style={{backgroundImage: "url('bg.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
       
-      <div className=" h-[80vh] w-[80vw] md:h-[70vh] md:w-[70vw] flex justify-center items-center m-auto backdrop-blur-xl rounded-3xl border-2 border-yellow-300">
+      <div className=" h-[50vh] w-[80vw] md:h-[70vh] md:w-[70vw] flex justify-center items-center m-auto backdrop-blur-xl rounded-3xl border-2 border-yellow-300">
         <div>
           <div className=" m-auto -mt-7 text-black text-center  font-bold text-6xl">
             Syahi
           </div>
-          <div className=" mt-5 m-auto w-[60vw] text-black md:text-3xl text-[17px] text-center">
+          <div className=" mt-5 m-auto w-[60vw] text-black md:text-3xl text-[17px] ">
             Syahi - the literary festival at KJSCE’s Symphony comprises poetry,
             storytelling, creative writing, workshops and the glorious open-mic
             'Mehfil-e-Shaam’ Syahi is home for everyone to express their
