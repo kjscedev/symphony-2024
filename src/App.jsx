17837cd6febe7775f891e5b8a91b8143e82549e4
kjscedev <kjscelive@somaiya.edu>
2025-02-13 13:46:24 +0530

    done with sponsors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -10,51 +9,24 @@ import Shield from "./pages/Shield";
 import Rules from "./pages/Rules";
 import Sponsors from "./pages/Sponsors.jsx";
 import Proshows from "./pages/Proshows";
-import React, { useState, useEffect } from "react";
-
-import "./App.css";
+import React from "react";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="flex flex-col min-h-screen">
-      {loading && (
-        <video
-          autoPlay
-          loop
-          muted
-          className="preloader-video absolute inset-0 object-cover w-full h-full"
-        >
-          <source src="/Preloader.mp4" type="video/mp4" />
-        </video>
-      )}
-      {!loading && (
-        <>
-          <Navbar />
-          <div>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/syahi" element={<Syahi />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/shield" element={<Shield />} />
-              <Route path="/sponsors" element={<Sponsors />} />
-              <Route path="/rules" element={<Rules />} />
-
-              <Route path="/proshows" element={<Proshows />} />
-            </Routes>
-          </div>
-          <Footer />
-        </>
-      )}
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/syahi" element={<Syahi />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/shield" element={<Shield />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/rules" element={<Rules />} />
+          <Route path="/proshows" element={<Proshows />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -1,96 +1,54 @@
-function Sponsors() {
-  return (
-    <div className="h-[100vh] w-[100vw] flex justify-center pt-[12vh]"
-    style={{backgroundImage: "url('bg.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-      {/* <video
-        className="absolute inset-0 object-cover w-full h-full"
-        autoPlay
-        loop
-        muted
+  function Sponsors() {
+    const sponsors = [
+      { src: "s1.jpg", link: "" },
+      { src: "s2.jpg", link: "https://www.dsgroup.com/our-brand/confectionery/pass-pass-pulse" },
+      { src: "s3.jpg", link: "" },
+      { src: "s4.jpg", link: "" },
+      { src: "s5.jpg", link: "" },
+      { src: "s6.jpg", link: "" },
+      { src: "s7.jpg", link: "" },
+      { src: "s8.jpg", link: "" },
+      { src: "s9.jpg", link: "" },
+      { src: "s10.jpg", link: "" },
+      { src: "s11.jpg", link: "" },
+      { src: "s12.jpg", link: "" },
+      { src: "s13.jpg", link: "" },
+      { src: "s14.jpg", link: "" },
+      { src: "s15.jpg", link: "https://www.bisleri.com" },
+      { src: "s16.jpg", link: "" },
+      { src: "s17.jpg", link: "" },
+      { src: "s18.jpg", link: "" },
+      { src: "s19.jpg", link: "" },
+      { src: "s20.jpg", link: "https://www.bisleri.com" },
+      { src: "s21.jpg", link: "" },
+      { src: "s22.jpg", link: "" },
+      { src: "s23.jpg", link: "" },
+      { src: "s24.jpg", link: "" },
+      { src: "s25.jpg", link: "" },
+    ];
+
+    return (
+      <div
+        className="h-[100vh] w-[100vw] flex justify-center pt-[12vh]"
+        style={{ backgroundImage: "url('bg.png')", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}
       >
-        <source src="bg loop.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video> */}
-      <div className=" h-[80vh] w-[80vw] md:h-[70vh] md:w-[70vw] gap-14 justify-center items-center m-auto backdrop-blur-xl rounded-3xl border-2 border-yellow-300 ">
-        {" "}
-        <div>
-          <div className=" font-Symphony text-black text-center mt-7 font-bold text-xl">
-            OUR SPONSORS
+        <div className="h-[80vh] w-[80vw] md:h-[70vh] md:w-[70vw] gap-14 justify-center items-center m-auto backdrop-blur-xl rounded-3xl border-2 border-yellow-300 p-5">
+          <div>
+            <div className="font-Symphony text-black text-center mt-3 font-bold text-xl">OUR SPONSORS</div>
+            <div className="h-[60vh] w-full grid grid-cols-2  md:grid-cols-3 gap-6 mt-6 overflow-y-auto scrollbar-hide">
+              {sponsors.map((sponsor, index) => (
+                <div key={index} className="h-[120px] w-[120px] flex justify-center items-center m-auto">
+                  <a href={sponsor.link} target="_blank" rel="noopener noreferrer">
+                    <img className="h-[120px] w-[120px] object-contain" src={sponsor.src} alt={`Sponsor ${index + 1}`} />
+                  </a>
+                </div>
+              ))}
+            </div>
+            <img className="absolute -bottom-10 -right-8 mobile-image" width={110} src="hme.png" alt="Decoration" />
           </div>
-          <div class=" h-[62vh] w-[70vw] md:h-[50vh] md:w-[60vw] grid grid-cols-2 md:grid-cols-3 gap-12 m-auto mt-9 overflow-auto ">
-            <div className=" h-[120px] m-auto">
-              <a href="https://www.thedanceworx.com/">
-                <img className="h-[120px]" src="s1.png" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="https://noescape.in/">
-                <img className="h-[120px]" src="s2.jpg" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="https://www.birdsofparadyes.com/">
-                <img className="h-[120px]" src="s3.png" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="https://envi.in/">
-                <img className="h-[120px]" src="s4.png" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="https://www.thedanceworx.com/">
-                <img className="h-[120px]" src="s5.jpeg" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="">
-                <img className="h-[120px]" src="s6.jpg" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="https://offthegrid.in/">
-                <img className="h-[120px]" src="s7.png" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="">
-                <img className="h-[120px]" src="s81.jpg" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="">
-                <img className="h-[120px]" src="s91.jpg" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="https://www.ekostay.com/">
-                <img className="h-[120px]" src="s10.jpg" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="https://www.powermaxfitness.co.in/">
-                <img className="h-[120px]" src="s12.jpg" />
-              </a>
-            </div>
-            <div className="h-[120px] m-auto">
-              <a href="https://www.instagram.com/cocomistry_/">
-                <img className="h-[120px]" src="s13.jpeg" />
-              </a>
-            </div>
-          </div>
-          {/* <div className=" text-white text-center mt-7 font-bold flex justify-center items-center absolute bottom-8 m-auto w-[80vw]  md:w-[70vw]  ">
-            
-          </div> */}
-          <img
-            className="absolute -bottom-10 -right-8 mobile-image "
-            width={110}
-            src="hme.png"
-          ></img>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Sponsors;
+  export default Sponsors;
