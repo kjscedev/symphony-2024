c3126156821cd1b87bf262946c0a4287f1e4128a
kjscedev <kjscelive@somaiya.edu>
2025-02-13 14:11:51 +0530

    added harsh

--- src/pages/Proshows.jsx
+++ src/pages/Proshows.jsx
@@ -6,21 +6,22 @@ const Proshows = () => {
       className="min-h-screen w-screen flex justify-center pt-[10vh] overflow-hidden bg-cover bg-center bg-repeat"
       style={{ backgroundImage: "url('bg.png')" }}
     >
-      <div className="relative w-[90vw] max-w-[1800px] h-auto flex flex-col md:flex-row md:gap-10 items-center justify-center mt-20 space-y-8 mb-10">
+      <div className="relative w-[90vw] max-w-[1800px] h-auto flex flex-col md:flex-2 gap-10 md:mt-20 md:gap-10 items-center justify-center mt-24 space-y-8 mb-10">
         {[
-          { title: "Javed Ali", date: "12th March", img: "Javed Ali.png" },
-          { title: "DJ Khushi", date: "13th March", img: "DJ khushi.png" },
-          { title: "LMI", date: "14th March", img: "LMI.png" },
+          { title: "Javed Ali", date: "12th February", img: "Javed Ali.png" },
+          { title: "DJ Khushi", date: "13th February", img: "DJ khushi.png" },
+          { title: "LMI", date: "14th February", img: "LMI.png" },
+          { title: "Harsh Gujral", date: "14th February", img: "harsh.png" },
         ].map((show, index) => (
           <div
             key={index}
             className="w-full max-w-[450px] h-[40vh] bg-black/50 backdrop-blur-lg rounded-3xl border-2 border-yellow-300 relative shadow-xl hover:scale-105 transition-transform duration-300 flex"
           >
-            <div className="p-6 text-white">
+            <div className="p-4 text-white">
               <h2 className="text-3xl font-bold">{show.title}</h2>
               <p className="text-lg mt-2 text-yellow-300">{show.date}</p>
             </div>
-            <div>
+            <div className="m-2">
               <img
                 className="absolute bottom-0 right-0 max-w-[220px] h-[350px] md:h-[400px] md:max-w-[250px]"
                 src={show.img}
